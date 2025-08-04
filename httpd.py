399af2bd786a050a27f4c33613e3ce5ce6cb12a8
Keiji <Ohira>
2025-08-04 14:28:37 +0900

    温度・湿度表示

--- httpd.py
+++ httpd.py
@@ -1,4 +1,3 @@
-
 def start_page(log_text=""):
     html = f"""<!DOCTYPE html>
 <html lang="en">
@@ -10,20 +9,27 @@ def start_page(log_text=""):
 <body>
     <div class="container">
         <h1>Pico Operation Page</h1>
-        <a href="/led/on">LED ON</a>
-        <a href="/led/off">LED OFF</a>
-        <a href="/play">Play Mario</a>
-        <a href="/stop">Stop Mario</a>
 
+        <div class="button-group">
+            <a href="/led/on">LED ON</a>
+            <a href="/led/off">LED OFF</a>
+            <a href="/play">Play Mario</a>
+            <a href="/stop">Stop Mario</a>
+        </div>
         <p>温度・湿度測定</p>
-        <a href="/measure_start">Measurement Start</a>
-        <a href="/measure_stop">Measurement Stop</a>
+        <div class="button-group">
+            <a href="/measure_start">Measurement Start</a>
+            <a href="/measure_stop">Measurement Stop</a>
+        </div>
 
-        <div style="margin-top: 20px;">
-            <label for="sensor" style="display:block; margin-bottom: 8px;">sensor data</label>
-            <textarea id="sensor" name="sensor" rows="30" cols="40" readonly>{log_text}</textarea>
+        <div class="sensor-block">
+            <p>sensor data</p>
+            <textarea id="sensor" name="sensor" rows="10" cols="40" readonly>{log_text}</textarea>
         </div>
     </div>
 </body>
 </html>"""
     return html
+
+
+
