b4e8118ed33cf490223a45a25a9634ef9fb26944
Keiji <Ohira>
2025-08-04 13:18:01 +0900

    レイアウト変更

--- httpd.py
+++ httpd.py
@@ -1,7 +1,8 @@
 def start_page(state):
-    html = f"""<!DOCTYPE html>
-<html>
+    html = """<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
     <title>Pico Web Server</title>
     <link rel="stylesheet" href="/style.css">
 </head>
@@ -12,6 +13,15 @@ def start_page(state):
         <a href="/led/off">LED OFF</a>
         <a href="/play">Play Mario</a>
         <a href="/stop">Stop Mario</a>
+
+        <p>温度・湿度測定</p>
+        <a href="/measure_start">Measurement Start</a>
+        <a href="/measure_stop">Measurement Stop</a>
+
+        <div style="margin-top: 20px;">
+            <label for="sensor" style="display:block; margin-bottom: 8px;">sensor data</label>
+            <input type="text" id="sensor" name="sensor" rows="30"size="30">
+        </div>
     </div>
 </body>
 </html>"""
